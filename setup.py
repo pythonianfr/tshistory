1a91eefb0cc9630f0bd1df107263c1dd345b4519
Aurelien Campeas <aurelien.campeas@pythonian.fr>
2017-04-24 12:37:45 +0200

    pkg: package tshistory 0.1

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+
+setup(name='tshistory',
+      version='0.1',
+      author='Pythonian',
+      author_email='aurelien.campeas@pythonian.fr, arnaud.campeas@pythonian.fr',
+      description='Store timeseries histories into postgres',
+
+      packages=['tshistory'],
+      package_dir={'tshistory': '.'},
+      install_requires=[
+          'pathlib',
+          'pandas',
+          'sqlalchemy',
+          'pytest_sa_pg',
+      ],
+      dependency_links=[
+          'hg+https://bitbucket.org/pythonian/pytest_sa_pg#egg=pytest_sa_pg-0',
+      ]
+)
