b59b326fd353fbc1536b31cc92ad8a016da57e38
Aurélien Campéas <aurelien.campeas@pythonian.fr>
2017-10-11 14:45:34 +0200

    schema: allow to rebuild the schema using a fresh metadata object
    This will help writing tests altering the schema.

--- tshistory/schema.py
+++ tshistory/schema.py
@@ -5,31 +5,38 @@ from sqlalchemy.dialects.postgresql import JSONB
 
 meta = MetaData()
 
-registry = Table(
-    'registry', meta,
-    Column('id', Integer, primary_key=True),
-    Column('name', String, index=True, nullable=False, unique=True),
-    Column('table_name', String, index=True, nullable=False, unique=True),
-    Column('metadata', JSONB(none_as_null=True))
-)
+def make_schema(meta):
+
+    registry = Table(
+        'registry', meta,
+        Column('id', Integer, primary_key=True),
+        Column('name', String, index=True, nullable=False, unique=True),
+        Column('table_name', String, index=True, nullable=False, unique=True),
+        Column('metadata', JSONB(none_as_null=True))
+    )
 
 
-changeset = Table(
-    'changeset', meta,
-    Column('id', Integer, primary_key=True),
-    Column('author', String, index=True, nullable=False),
-    Column('insertion_date', DateTime, index=True, nullable=False),
-    Column('metadata', JSONB(none_as_null=True))
-)
+    changeset = Table(
+        'changeset', meta,
+        Column('id', Integer, primary_key=True),
+        Column('author', String, index=True, nullable=False),
+        Column('insertion_date', DateTime, index=True, nullable=False),
+        Column('metadata', JSONB(none_as_null=True))
+    )
 
-changeset_series = Table(
-    'changeset_series', meta,
-    Column('csid', Integer, ForeignKey('changeset.id'),
-           index=True, nullable=False),
-    Column('serie', String, ForeignKey('registry.name'),
-           index=True, nullable=False),
-    PrimaryKeyConstraint('csid', 'serie', name='changeset_series_pk'),
-)
+    changeset_series = Table(
+        'changeset_series', meta,
+        Column('csid', Integer, ForeignKey('changeset.id'),
+               index=True, nullable=False),
+        Column('serie', String, ForeignKey('registry.name'),
+               index=True, nullable=False),
+        PrimaryKeyConstraint('csid', 'serie', name='changeset_series_pk'),
+    )
+
+    return registry, changeset, changeset_series
+
+
+registry, changeset, changeset_series = make_schema(meta)
 
 
 def init(engine):
