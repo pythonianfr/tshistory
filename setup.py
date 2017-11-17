33298fbf3a468efbfefff2c367c08c7d3bf39580
Aurelien Campeas <aurelien.campeas@pythonian.fr>
2017-11-17 17:37:56 +0100

    pkg: prepare 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='tshistory',
-      version='0.1.0',
+      version='0.1.1',
       author='Pythonian',
       author_email='aurelien.campeas@pythonian.fr, arnaud.campeas@pythonian.fr',
       url='https://bitbucket.org/pythonian/tshistory',
