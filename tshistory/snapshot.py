c25c5cf559e565d5e33ade8e522d48e11317d55c
Aurelien Campeas <aurelien.campeas@pythonian.fr>
2019-05-10 18:11:45 +0200

    snapshot: silence a pandas warning

--- tshistory/snapshot.py
+++ tshistory/snapshot.py
@@ -40,7 +40,7 @@ class Snapshot(SeriesServices):
         if ts is None:
             return None
         # use `view` as a workarround for "cannot include dtype 'M' in a buffer"
-        indexes = np.ascontiguousarray(ts.index).view(np.uint8).data.tobytes()
+        indexes = np.ascontiguousarray(ts.index.values).view(np.uint8).data.tobytes()
         indexes_size = struct.pack('!L', len(indexes))
 
         if self.isstr:
