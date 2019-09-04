8d70c6b249fc02a6e2e220d05d7caa05add7016f
Aurelien Campeas <aurelien.campeas@pythonian.fr>
2019-09-04 16:51:24 +0200

    retire obsolete module

--- tshistory/repair.py
+++
@@ -1,6 +0,0 @@
-
-def repair_bad_serie(tsh, engine, seriename, reparator, rewriter):
-    logs = tsh.log(engine, names=(seriename,), diff=True)
-    for idx, log in enumerate(logs):
-        reparator(tsh, engine, log)
-        rewriter(tsh, engine, idx, log)
