1a785a5b73d4b2ab172c5ede4b95a697944f08cf
Aurelien Campeas <aurelien.campeas@pythonian.fr>
2020-02-06 18:20:28 +0100

    tests/staircase: exhibit a weird bug related to tz-awareness

--- test/test_tsio.py
+++ test/test_tsio.py
@@ -1398,6 +1398,44 @@ def test_staircase_2_tznaive(engine, tsh):
     assert isinstance(deltas, pd.Series)
 
 
+def test_staircase_tzaware_funny_bug(engine, tsh):
+    # maybe a more interesting example, each days we insert 7 data points
+    for idx, idate in enumerate(pd.date_range(start=utcdt(2015, 1, 1),
+                                              end=utcdt(2015, 1, 4),
+                                              freq='D')):
+        ts = genserie(start=idate, freq='H', repeat=7)
+        tsh.update(engine, ts, 'funny-staircase', 'test', insertion_date=idate)
+
+    deltas = tsh.staircase(
+        engine,
+        'funny-staircase',
+        delta=timedelta(hours=3),
+        from_value_date=utcdt(2015, 1, 2),
+        to_value_date=utcdt(2015, 1, 3)
+    )
+    assert_df("""
+2015-01-02 03:00:00+00:00    3.0
+2015-01-02 04:00:00+00:00    4.0
+2015-01-02 05:00:00+00:00    5.0
+2015-01-02 06:00:00+00:00    6.0
+""", deltas)
+
+    with pytest.raises(Exception) as err:
+        deltas = tsh.staircase(
+            engine,
+            'funny-staircase',
+            delta=timedelta(hours=3),
+            from_value_date=utcdt(2015, 1, 2)
+        )
+
+    assert err.value.args[0].startswith(
+        '(psycopg2.errors.AmbiguousFunction) '
+        'function pg_catalog.overlaps(timestamp with time zone, unknown, '
+        'timestamp without time zone, timestamp without time zone) '
+        'is not unique'
+    )
+
+
 def test_rename(engine, tsh):
     if tsh.namespace == 'zzz':
         return  # this test can only run once
