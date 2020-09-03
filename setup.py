27c3549579cec6ac55ee849cbc230114cba95784
Aurelien Campeas <aurelien.campeas@pythonian.fr>
2020-09-03 18:40:24 +0200

    pkg: prepare 0.12.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ doc = Path(__file__).parent / 'README.md'
 
 
 setup(name='tshistory',
-      version='0.12.1',
+      version='0.12.2',
       author='Pythonian',
       author_email='aurelien.campeas@pythonian.fr, arnaud.campeas@pythonian.fr',
       url='https://hg.sr.ht/~pythonian/tshistory',
