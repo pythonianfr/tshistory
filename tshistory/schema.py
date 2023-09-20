a185d7dab70e1316ed163c3e7f6df718858b4b2e
Aurélien Campéas <aurelien.campeas@pythonian.fr>
2023-09-20 12:00:19 +0200

    schema: simplify the group creation

--- tshistory/schema.py
+++ tshistory/schema.py
@@ -21,7 +21,7 @@ class tsschema(object):
 
     def create(self, engine, reset=False):
         self._create_series(engine, self.namespace)
-        self._create_group(engine, reset=reset)
+        self._create_group(engine)
 
     def _create_series(self, engine, namespace):
         with engine.begin() as cn:
@@ -36,24 +36,11 @@ class tsschema(object):
             cn.execute(sqlfile(SERIES, ns=namespace))
         self._create_kvstore(engine, namespace)
 
-    def _create_group(self, engine, reset=False):
-        # cleanup
-        exists = engine.execute(
-            'select 1 from information_schema.schemata where schema_name = %(name)s',
-            name=f'{self.namespace}.group'
-        ).scalar()
-        if exists and not reset:
-            print(f'The "{self.namespace}.group" namespace already exists.')
-            return
-
-        if reset:
-            # undo the contents of group.sql
-            with engine.begin() as cn:
-                cn.execute(f'drop table if exists "{self.namespace}".group_registry')
-                cn.execute(f'drop table if exists "{self.namespace}".groupmap')
-
+    def _create_group(self, engine):
         with engine.begin() as cn:
             cn.execute(f'drop schema if exists "{self.namespace}.group" cascade')
+            cn.execute(f'drop table if exists "{self.namespace}".group_registry')
+            cn.execute(f'drop table if exists "{self.namespace}".groupmap')
 
         # creation
         # dedicated time series store for the groups
