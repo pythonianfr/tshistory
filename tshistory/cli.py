0779710a4fb4b6c0c7290d3958ad1decb33aa719
Aurélien Campéas <aurelien.campeas@pythonian.fr>
2023-10-16 18:13:43 +0200

    cli/migrate_to_groups: fix it

--- tshistory/cli.py
+++ tshistory/cli.py
@@ -41,12 +41,11 @@ def fix_groups_metadata_(db_uri, deletebroken=False, namespace='tsh'):
 
 @tsh.command(name='migrate-to-groups')
 @click.argument('db-uri')
-@click.option('--reset', is_flag=True, default=False)
 @click.option('--namespace', default='tsh')
-def migrate_to_groups(db_uri, reset=False, namespace='tsh'):
+def migrate_to_groups(db_uri, namespace='tsh'):
     engine = create_engine(find_dburi(db_uri))
     sch = tsschema(namespace)
-    sch._create_group(engine, reset)
+    sch._create_group(engine)
 
 
 # db maintenance
