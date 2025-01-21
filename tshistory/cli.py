a02cdc1ce56b7e2cedb0b15bc8163bd913bf87a5
Aurélien Campéas <aurelien.campeas@pythonian.fr>
2025-01-21 19:38:05 +0100

    cli: remove obsolete commands

--- tshistory/cli.py
+++ tshistory/cli.py
@@ -1,5 +1,4 @@
 from collections import defaultdict
-from random import sample
 
 from pkg_resources import iter_entry_points
 import click
@@ -8,10 +7,8 @@ from sqlalchemy import create_engine
 from dbcache import api as storeapi
 from tshistory.api import timeseries
 from tshistory.config import configuration
-from tshistory.util import (
-    checkdiffs_for_name,
-    run_migrations
-)
+from tshistory.util import run_migrations
+
 from tshistory.schema import tsschema
 
 
@@ -23,30 +20,6 @@ def tsh():
 @tsh.command()
 def configpath():
     print(configuration.path())
-
-
-# migration
-
-@tsh.command(name='fix-primary-groups-metadata')
-@click.argument('db-uri')
-@click.option('--deletebroken', default=False, is_flag=True)
-@click.option('--namespace', default='tsh')
-def fix_groups_metadata_(db_uri, deletebroken=False, namespace='tsh'):
-    engine = create_engine(configuration().find_dburi(db_uri))
-
-    from tshistory.migrate import fix_groups_metadata
-    fix_groups_metadata(
-        engine, namespace, interactive=True, deletebroken=deletebroken
-    )
-
-
-@tsh.command(name='migrate-to-groups')
-@click.argument('db-uri')
-@click.option('--namespace', default='tsh')
-def migrate_to_groups(db_uri, namespace='tsh'):
-    engine = create_engine(configuration().find_dburi(db_uri))
-    sch = tsschema(namespace)
-    sch._create_group(engine)
 
 
 # db maintenance
@@ -83,37 +56,6 @@ def dbversions(db_uri, namespace='tsh'):
     )
     for k, v in sorted(store.all().items()):
         print(f'{k} -> {v}')
-
-
-@tsh.command(name='checkdiffs')
-@click.argument('db-uri')
-@click.argument('name')
-@click.option('--namespace', default='tsh')
-def checkdiffs(db_uri, name, namespace='tsh'):
-    uri = configuration().find_dburi(db_uri)
-    tsa = timeseries(uri, namespace)
-    engine = create_engine(uri)
-
-    checkdiffs_for_name(engine, tsa, name)
-
-
-@tsh.command(name='random-checkdiffs')
-@click.argument('db-uri')
-@click.argument('number', type=int)
-@click.option('--namespace', default='tsh')
-def random_checkdiffs(db_uri, number, namespace='tsh'):
-    uri = configuration().find_dburi(db_uri)
-    tsa = timeseries(uri, namespace)
-    engine = create_engine(uri)
-
-    list_series = tsa.find('(by.and (by.source "local") (by.not (by.formula)))')
-
-    if len(list_series):
-        random_series = sample(list_series, int(number))
-
-        for name in random_series:
-            print("check starting for: ", name)
-            checkdiffs_for_name(engine, tsa, name)
 
 
 @tsh.command(name='shell')
